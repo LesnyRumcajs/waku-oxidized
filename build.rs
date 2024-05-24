4b36e79e2e0d2a8eda63979aa1eb1e260d04b7ef
Hubert Bugaj <lesny.rumcajs@gmail.com>
2024-05-24 20:50:31 +0200

    fix more

--- build.rs
+++ build.rs
@@ -1,9 +1,7 @@
 fn main() {
-    tonic_build::compile_protos("proto/filter.proto").unwrap();
-    tonic_build::compile_protos("proto/light_push.proto").unwrap();
-    tonic_build::compile_protos("proto/peer_exchange.proto").unwrap();
     tonic_build::configure()
         .build_server(false)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(
             &[
                 "proto/message.proto",
